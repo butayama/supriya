81f10f72e4886101ef99d30197ea4ea77f153e3d
Josiah Oberholtzer <josiah.oberholtzer@gmail.com>
2020-12-12 16:21:09 -0500

    Add noise imports

--- supriya/newpatterns/__init__.py
+++ supriya/newpatterns/__init__.py
@@ -1,7 +1,11 @@
+from .noise import ChoicePattern, RandomPattern, ShufflePattern
 from .patterns import BinaryOpPattern, Pattern, SequencePattern
 
 __all__ = [
     "BinaryOpPattern",
+    "ChoicePattern",
     "Pattern",
+    "RandomPattern",
     "SequencePattern",
+    "ShufflePattern",
 ]
